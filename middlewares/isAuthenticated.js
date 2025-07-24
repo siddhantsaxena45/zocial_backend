e38ea700d5824fa5559cbe569f1d9b6cf483b130
Siddhant Saxena <siddhant.saxena.2004@gmail.com>
2025-07-24 11:24:51 +0530

    logout fix

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = async(req, res, next)=> {
+const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized", success: false });
+            return res.redirect(401, "/login");
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.id = decoded.userId;
         next();
-     }
+    }
     catch (error) {
         console.log(error);
     }
